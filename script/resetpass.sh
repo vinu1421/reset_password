1e79f0c03189b11ca07100bd8cb8291615a75eec
vinayak <vinayak.prasannakumar@mindcurv.com>
2020-08-26 20:51:37 +0530

    added script

---
+++ script/resetpass.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#environment=$1
+#username=$2
+#password=$(mkpasswd -l 12 2> /dev/null)
+#url1="http://192.168.56.102:9200"
+
+#if [[ $environment == "pro" ]]; then
+
+#curl -k --user elastic:gV2okuT65fJwP3fkrOyd -XPOST ${url1}/_security/user/${username}/_password?pretty -H 'Content-Type: application/json' -d'{"password" : "'"${password}"'"}}'
+
+#else
+
+#curl -k --user elastic:gV2okuT65fJwP3fkrOyd -XPOST ${url1}/_security/user/${username}/_password?pretty -H 'Content-Type: application/json' -d'{"password" : ${password}}'
+
+#fi
+
+#echo $password
+
+echo $Username
+echo $Environment
