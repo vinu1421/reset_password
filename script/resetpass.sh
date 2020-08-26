4b4fa4b81d75f9e4be7fb987534cc66b5a3a5efb
vinayak <vinayak.prasannakumar@mindcurv.com>
2020-08-26 21:10:55 +0530

    final script

--- script/resetpass.sh
+++ script/resetpass.sh
@@ -1,20 +1,17 @@
-#!/bin/bash
-#environment=$1
-#username=$2
-#password=$(mkpasswd -l 12 2> /dev/null)
-#url1="http://192.168.56.102:9200"
+!/bin/bash
+environment=$Environment
+username=$Username
+password=$(mkpasswd -l 12 2> /dev/null)
+url1="http://192.168.56.102:9200"
 
-#if [[ $environment == "pro" ]]; then
+if [[ $environment == "pro" ]]; then
 
-#curl -k --user elastic:gV2okuT65fJwP3fkrOyd -XPOST ${url1}/_security/user/${username}/_password?pretty -H 'Content-Type: application/json' -d'{"password" : "'"${password}"'"}}'
+curl -k --user elastic:gV2okuT65fJwP3fkrOyd -XPOST ${url1}/_security/user/${username}/_password?pretty -H 'Content-Type: application/json' -d'{"password" : "'"${password}"'"}}'
 
-#else
+else
 
-#curl -k --user elastic:gV2okuT65fJwP3fkrOyd -XPOST ${url1}/_security/user/${username}/_password?pretty -H 'Content-Type: application/json' -d'{"password" : ${password}}'
+curl -k --user elastic:gV2okuT65fJwP3fkrOyd -XPOST ${url1}/_security/user/${username}/_password?pretty -H 'Content-Type: application/json' -d'{"password" : ${password}}'
 
-#fi
+fi
 
-#echo $password
-
-echo $Username
-echo $Environment
+echo $password
